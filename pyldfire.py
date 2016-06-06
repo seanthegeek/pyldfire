eea397633c113482860b1ed3c80356c9ce65b5c2
Sean Whalen <whalenster@gmail.com>
2016-06-05 22:28:01 -0400

    Add docstrings

--- pyldfire.py
+++ pyldfire.py
@@ -29,14 +29,15 @@ def list_to_file(l):
     return StringIO('\n'.join(l))
 
 
-class WildFireException(Exception):
+class WildFireException(RuntimeError):
+    """This exception is raised when an API error occurs"""
     pass
 
 
 class WildFire(object):
     errors = {
         401: "API key is invalid",
-        403: "Permission denied",
+        403: "Permission denied. This can occur when attempting to download benign or greyware samples.",
         404: "Not found",
         405: "Method other than POST used",
         413: "Sample file size over max limit",
@@ -57,7 +58,8 @@ class WildFire(object):
     }
 
     @staticmethod
-    def raise_errors(response, *args, **kwargs):
+    def _raise_errors(response, *args, **kwargs):
+        """Callback hook for raising API errors"""
         if response.headers['content-type'].lower() == "text/xml":
             results = xmltodict.parse(response.text)
             if "error" in results.keys():
@@ -66,16 +68,41 @@ class WildFire(object):
             raise WildFireException(WildFire.errors[response.status_code])
 
     def __init__(self, api_key, host="wildfire.paloaltonetworks.com", proxies=None, verify=True):
+        """Initializes the WildFire class
+
+        Args:
+            api_key (str): A WildFire API Key
+            host (str): The hostname of the WildFire service or appliance
+            proxies (dict): An optional dictionary containing proxy data, with https as the key, and the proxy path
+            as the value
+            verify (bool): Verify the certificate
+            verify (str): A path to a CA cert bundle
+        """
         self.api_key = api_key
         self.host = host
         self.api_root = "https://{0}{1}".format(self.host, "/publicapi")
         self.session = Session()
         self.session.proxies = proxies
         self.session.verify = verify
-        self.session.hooks = dict(response=WildFire.raise_errors)
+        self.session.hooks = dict(response=WildFire._raise_errors)
         self.session.headers.update({"User-Agent": "pyldfire/{0}".format(__version__)})
 
     def get_verdict(self, file_hashes):
+        """Gets the verdict for one or more samples
+
+        Args:
+            file_hashes (list): A list of file hash strings
+            file_hashes (str): A single file hash
+
+        Returns:
+            str: If a single file hash is passed, a string containing the verdict
+            list: If multiple hashes a passed, a list of dictionaries containing information for each sample, including
+            the verdict.
+
+        Raises:
+            WildFireException: If an API error occurs
+
+        """
         multi = False
         if type(file_hashes) == list:
             if len(file_hashes) == 1:
@@ -101,6 +128,19 @@ class WildFire(object):
         return results
 
     def submit_file(self, file_obj, filename="sample"):
+        """Submits a file to WildFire for analysis
+
+        Args:
+            file_obj (file): The file to send
+            filename (str): An optional filename
+
+        Returns:
+            dict: Analysis results
+
+        Raises:
+             WildFireException: If an API error occurs
+
+        """
         url = "{0}{1}".format(self.api_root, "/submit/file")
         data = dict(apikey=self.api_key)
         files = dict(file=(filename, file_obj))
@@ -109,6 +149,25 @@ class WildFire(object):
         return xmltodict.parse(response.text)['wildfire']['upload-file-info']
 
     def submit_remote_file(self, url):
+        """Submits a file from a remote URL for analysis
+
+        Notes:
+            This is for submitting files located at remote URLs, not web pages.
+
+        See Also:
+            WildFire.submit_url(self, url)
+
+        Args:
+            url (str): The URL where the file is located
+
+        Returns:
+            dict: Analysis results
+
+        Raises:
+             WildFireException: If an API error occurs
+
+
+        """
         request_url = "{0}{1}".format(self.api_root, "/submit/url")
         data = dict(apikey=self.api_key, url=url)
         response = self.session.post(request_url, data=data)
@@ -116,6 +175,20 @@ class WildFire(object):
         return xmltodict.parse(response.text)['wildfire']['upload-file-info']
 
     def submit_url(self, urls):
+        """
+        Submits a URL to a web page for analysis
+        Args:
+            urls (str): A single URL
+            urls (list): A list of URLs
+
+        Returns:
+            dict: IF a single URL is passed, a dictionary of analysis results
+            list: If multiple URLs are passed, a list of corresponding dictionaries containing analysis results
+
+        Raises:
+             WildFireException: If an API error occurs
+
+        """
         multi = False
         if type(urls) == list:
             if len(urls) == 1:
@@ -140,6 +213,20 @@ class WildFire(object):
         return results
 
     def _get_report(self, file_hash, report_format, stream=False):
+        """An internal method for retrieving analysis reports
+        Args:
+            file_hash (str):  A hash of a sample
+            report_format (str): either xml or pdf
+            stream (bool): Stream the HTTP download. Useful for binary data.
+
+        Returns:
+            dict: Analysis results
+            bytes: PDF bytes
+
+        Raises:
+             WildFireException: If an API error occurs
+
+        """
         request_url = "{0}{1}".format(self.api_root, "/get/report")
         data = dict(apikey=self.api_key, hash=file_hash, format=report_format)
         response = self.session.post(request_url, data=data, stream=stream)
@@ -151,18 +238,71 @@ class WildFire(object):
         return response
 
     def get_report(self, file_hash):
+        """Gets analysis results as structured data
+        Args:
+            file_hash (str(: A hash of a sample
+
+        Returns:
+            dict: Analysis results
+
+        """
         return self._get_report(file_hash, 'xml')
 
     def get_pdf_report(self, file_hash):
+        """GGets analysis results as a PDF
+        Args:
+            file_hash: A hash of a sample of a file
+
+        Returns:
+            bytes: The PDF bytes
+
+        Raises:
+             WildFireException: If an API error occurs
+        """
         return self._get_report(file_hash, 'pdf', stream=True)
 
     def get_sample(self, file_hash):
+        """Gets a sample file
+        Args:
+            file_hash (str): A hash of a sample
+
+        Returns:
+            bytes: The sample file
+
+        Raises:
+             WildFireException: If an API error occurs
+        """
         request_url = "{0}{1}".format(self.api_root, "/get/sample")
         data = dict(apikey=self.api_key, hash=file_hash)
 
         return self.session.post(request_url, data=data, stream=True).content
 
     def get_pcap(self, file_hash, platform=None):
+        """Gets a pcap from a sample analysis
+            Args:
+                file_hash (str): A hash of a sample
+                platform (int): One of the following integers:
+
+                1: Windows XP, Adobe Reader 9.3.3, Office 2003
+
+                2: Windows XP, Adobe Reader 9.4.0, Flash 10, Office 2007
+
+                3: Windows XP, Adobe Reader 11, Flash 11, Office 2010
+
+                4: Windows 7 32-bit, Adobe Reader 11, Flash 11, Office 2010
+
+                5: Windows 7 64bit, Adobe Reader 11, Flash 11, Office 2010
+
+                50: Mac OS X Mountain Lion
+
+                201: Android 2.3, API 10, avd2.3.
+
+            Returns:
+                bytes: The PCAP bytes
+
+            Raises:
+                 WildFireException: If an API error occurs
+            """
         request_url = "{0}{1}".format(self.api_root, "/get/pcap")
         data = dict(apikey=self.api_key, hash=file_hash)
         if platform is not None:
@@ -171,4 +311,9 @@ class WildFire(object):
         return self.session.post(request_url, data=data, stream=True).content
 
     def get_malware_test_file(self):
+        """Gets a unique, benign malware test file that will trigger an alert on Palo Alto Networks' firewalls
+
+        Returns:
+            bytes: Malware test file bytes
+        """
         return self.session.get("{0}{1}".format(self.api_root, "/test/pe"), stream=True).content
